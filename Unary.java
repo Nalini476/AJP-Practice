2b8ea389fe8894314a05fd2a9d79bbb08e9f6b98
Nalini476 <naniireddy474@gmail.com>
2025-06-02 17:06:01 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,12 @@
+public class Unary {
+public static void main(String[] args) {
+    int num1=10;
+    int num2=5;
+    System.out.println("sum="+(num1+num2));
+    System.out.println("Difference="+(num1-num2));
+    System.out.println("Product="+(num1*num2));
+    System.out.println("Division="+(num1/num2));
+    System.out.println("Reminder="+(num1%num2));
+}
+    
+}
