30a27b6f4818a4b9d4ac0ba2232fe26f3140d583
Nalini476 <naniireddy474@gmail.com>
2025-07-08 17:22:02 +0530

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,9 +1,7 @@
 public class Arithmetic {
     public static void main(String[] args) {
         int num1=10;
-        //pre increment
         int num2=++num1;
-        //post increment
         int num3=num1++;
         System.out.println("num1="+num1+" num2="+num2+" num3="+num3);
         //
