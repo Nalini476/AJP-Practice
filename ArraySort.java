f8368efb2c2401e3debc0c37d0f9cf78439be4d9
Nalini476 <naniireddy474@gmail.com>
2025-07-08 17:18:41 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -4,7 +4,6 @@ public class ArraySort {
         int arr[] = {2, 0, 9, 5, 1, 8};
         int temp;
 
-        // Sorting using simple selection sort or bubble sort logic
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
@@ -16,7 +15,6 @@ public class ArraySort {
             }
         }
 
-        // Print sorted array
         System.out.println("Sorted array:");
         for (int n : arr) {
             System.out.print(n + " ");
